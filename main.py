ed25ecd6266f567333b246819cb086321c3175a8
Param Nautiyal <pamail.7908@gmail.com>
2024-10-04 17:45:07 +0530

    feat:code updated with mongo pipeline to optimize performance

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__=='__main__':
     try:
         csv_to_mongo('Sleep', './data_csv/sleep_data.csv')
         csv_to_mongo('Activity', './data_csv/activity_data_csv.csv')
-        #getting today date in Iso format
+        # getting today date in Iso format
         today = str(datetime.now().isoformat())
         print(f"Fetching Data for {today}")
         transformer(today)
